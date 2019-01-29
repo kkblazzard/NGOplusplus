8a3d54045a1d3803b4fad756fb4490aed92b5493
Kay Sugiyama <keifuku.sugiyama@gmail.com>
2019-01-28 16:24:32 -0800

    home page styling updates

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OrgdetailsComponent } from './orgdetails/orgdetails.component';
 import { OrgregComponent } from './orgreg/orgreg.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  // { path: '', pathMatch: 'full', redirectTo: '/home'},
+  {path: "", component: HomeComponent},
   {path: "orgs/details", component: OrgdetailsComponent},
   {path: "orgs/registration", component: OrgregComponent},
 ];
