1fc9c95b4f3593be750a1a9f03014de0b7784d19
Kevin Blazzard <kkblazzard@mac.com>
2019-01-28 16:29:08 -0800

    New Org form

--- server/models.js
+++ server/models.js
@@ -1,10 +1,23 @@
 var mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost:27017/QuoteRankdb', function(err){
+mongoose.connect('mongodb://localhost:27017/NGOplusplusdb', function(err){
         console.log("Connected to DB");
         if(err){console.log(err);}});
-var productSchema = new mongoose.Schema({
-        author:{type: String, required:true, minlength:4},
-        quotes:[{quote:{type: String, minlength:4}, vote:{type: Number, default:0}}],
+var orgSchema = new mongoose.Schema({
+        name:{type: String, required:true, minlength:4},
+        mission:{type:String},
+        logo:{type:String},
+        admins:[{userId:String}],
+        events:[{eventId:String}],
+        webAddress:{webAddress:String},
+        fbAddress:{fbAddress:String},
+        twAddress:{twAddress:String},
+        street:{type:String},
+        city:{type:String},
+        State:{type:String},
+        zip:{type:Number},
+        phone:{type:String},
+        email:{type:String},
+        ein:{type:Number, minlength:9, maxlength:9}
         },
         {timestamps:true},);
-        module.exports=mongoose.model('product', productSchema);
+        module.exports=mongoose.model('org', orgSchema);
