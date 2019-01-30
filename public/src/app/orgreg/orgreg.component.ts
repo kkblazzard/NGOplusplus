a2b6702ce9ba1d08897bd0ff29448fb3a9ee1984
Kay Sugiyama <keifuku.sugiyama@gmail.com>
2019-01-30 12:41:46 -0800

    userID check to add new org, add admin id and org id

--- public/src/app/orgreg/orgreg.component.ts
+++ public/src/app/orgreg/orgreg.component.ts
@@ -12,51 +12,81 @@ export class OrgregComponent implements OnInit {
   constructor(private _httpService: HttpService,
     private _route: ActivatedRoute,
     private _router: Router) { }
-    Errors:any;
-    newOrg:any="";
-    id:any;
-    step1:boolean=true;
-    step2:boolean=false;
-    step3:boolean=false;
-    step:string="1";
-    ngOnInit() {
-      this.newOrg={
-        name:"", 
-        mission:"",
-        ein: Number,
-        phone:"",
-        email:"",  
-        logo:"", 
-        admins:[], 
-        events:[], 
-        webAddress:"", 
-        fbAddress:"", 
-        twAddress:"",
-        street:"",
-        city:"", 
-        state:"", 
-        zip:Number,
-      };
+    
+  Errors:any;
+  newOrg:any="";
+  orgId:any;
+  step1:boolean=true;
+  step2:boolean=false;
+  step3:boolean=false;
+  step:string="1";
+
+  userID: string;
+  updateUser: {};
+
+  ngOnInit() {
+    this.newOrg={
+      name:"", 
+      mission:"",
+      ein: Number,
+      phone:"",
+      email:"",  
+      logo:"", 
+      admins:[], 
+      events:[], 
+      webAddress:"", 
+      fbAddress:"", 
+      twAddress:"",
+      street:"",
+      city:"", 
+      state:"", 
+      zip:Number,
+    };
+
+    if(localStorage.getItem('loginUserID')){
+      this.userID = localStorage.getItem('loginUserID');
+      console.log("logged in user ID:", this.userID);
+    }
+    else{
+      console.log("there's no loginUserID");
+      this._router.navigate(['/user']);
+    }
   }
+  
   newOrgSubmission(){
-    console.log(this.newOrg);
+    this.newOrg.admins.push(this.userID);
+    console.log("after form fill and userid: ", this.newOrg);
+
     this._httpService.addOrg(this.newOrg)
     .subscribe(data=>{
-      this.id=data["_id"];
+      this.orgId=data["_id"];
       console.log("new org added to db",data);
+
       if (data['errors']){
         this.Errors = data['errors'];
           console.log("This is data['errors']");
           console.log(data['errors'])
       }
       else{
+        this.updateUser = { "orgId" : this.orgId };
+        this._httpService.userUpdate(this.userID, this.updateUser)
+        .subscribe(data => {
+          if(data['errors']){
+            console.log(data['errors'])
+          }
+          else{
+            console.log("userschema's orgid updated");
+          }
+        })
+        
         this.godetails();
       }
     });
   }
   godetails(){
-    this._router.navigate(['orgs/details/'+ this.id]);
+    this._router.navigate(['orgs/details/'+ this.orgId]);
   }
+
   step1switch(){
     this.step1=true;
     this.step2=false;
@@ -74,4 +104,5 @@ export class OrgregComponent implements OnInit {
     this.step3=true;
     this.step="3";
   }
+
 }
